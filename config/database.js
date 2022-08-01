80e5c758f936ddbc8be857e24b065e54cc95fb78
Md. Moniruzzaman <monir.jbt@gmail.com>
2022-08-02 04:14:33 +0600

    Updated String

--- config/database.js
+++ config/database.js
@@ -1,15 +1,21 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
+const parse = require("pg-connection-string").parse;
 
+const { host, port, database, user, password } = parse(
+  "postgresql://doadmin:AVNS_tL4BIk4dCRcIm8WOvS7@strapi-prod-do-user-4657530-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
+);
+module.exports = () => ({
+  connection: {
+    client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     debug: false,
   },
 });
-
